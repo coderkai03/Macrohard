260bea014d510b6473ce618b7ea65d398f397e22
Riankai1073 <riancorci@gmail.com>
2023-07-07 21:41:21 -0700

    refactored Prompt to View

--- Main.py
+++ Main.py
@@ -1,14 +1,14 @@
 from Customer import Profile
-from Prompt import Prompt
+from View import View
 
 #test get_customer_info
 #use return value to create a new Profile object
-c1 = Profile(**Prompt.get_customer_info())
+c1 = Profile(**View.get_customer_info())
 
 #print customer's info
 print(c1)
 
 #test validate_input
 print('Enter a number between 1 and 10.')
-num = Prompt.validate_input(1, 10)
+num = View.validate_input(1, 10)
 print(f'You entered {num}')
