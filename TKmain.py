b18e96d9b0fad1a1cff099a6bea8829ed0b5035b
Riankai1073 <riancorci@gmail.com>
2023-07-25 14:08:30 -0700

    testing computer items display

--- TKmain.py
+++ TKmain.py
@@ -1,35 +1,36 @@
 from tkinter import *
 from Store import Macrohard
+import tkinter.font as tkFont
 
 store = Macrohard('Rian')
 
 root = Tk()
-#root.geometry('800x600')
+root.title('Macrohard')
 
-# welcome = Label(frame1, text='Welcome to Macrohard!')
+print(1 if 'Space Mono' in tkFont.families() else 0)
+
+root.geometry('800x600')
+
+# welcome = Label(items1, text='Welcome to Macrohard!')
 # welcome.pack()
 
-frame1 = Frame(root)
-frame1.grid(row=1, column=0)
+items1 = Frame(root)
+items1.grid(row=1, column=0)
 
-frame2 = Frame(root)
-frame2.grid(row=1, column=1)
+# prices1 = Frame(root)
+# prices1.grid(row=1, column=1)
 
 comps = store.allprods.computers
+r=0
 for i in comps:
     prod = Checkbutton(
-        frame1,
-        text=i[0],
+        items1,
+        text=f'{i[0]:<50}${i[1]:>10.2f}',
+        font=tkFont.Font(family='Space Mono', size=10),
         onvalue=1,
         offvalue=0,
-    ).pack(anchor='w')
-    price = Label(
-        frame2,
-        text=i[1],
-    ).pack(anchor='e')
-
-# for i in comps:
-#     #print(i)
+    ).pack(anchor='w')#.grid(row=r, column=0)
+    r+=1
     
 
 root.mainloop()
