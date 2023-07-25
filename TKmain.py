b712685bb443f2519a7bb5b27583186384c642d0
Riankai1073 <riancorci@gmail.com>
2023-07-25 16:20:17 -0700

    made checkbox menu for computer items

--- TKmain.py
+++ TKmain.py
@@ -17,19 +17,46 @@ root.geometry('800x600')
 items1 = Frame(root)
 items1.grid(row=1, column=0)
 
-# prices1 = Frame(root)
-# prices1.grid(row=1, column=1)
+incrementers1 = Frame(root)
+incrementers1.grid(row=1, column=1)
 
+def showIncrementItem(item):
+    idx = prod_idxs.index(item)
+    print(f'{idx} found')
+    quantities[idx].configure(state='normal') if item.get() else quantities[idx].configure(state='disabled')
+
+#add to ProductList.py
 comps = store.allprods.computers
 r=0
+prod_idxs = []
+prods = []
+
+
+quantities = []
+quant_idxs = []
 for i in comps:
-    prod = Checkbutton(
+    prod_idxs.append(IntVar())
+    prods.append(Checkbutton(
         items1,
-        text=f'{i[0]:<50}${i[1]:>10.2f}',
+        text=f'{i[0]:<30}${i[1]:>10.2f}',
         font=tkFont.Font(family='Space Mono', size=10),
+        variable=prod_idxs[r],
         onvalue=1,
         offvalue=0,
-    ).pack(anchor='w')#.grid(row=r, column=0)
+        state='normal',
+    ))
+
+    quant_idxs.append(IntVar())
+    quantities.append(Entry(
+        incrementers1,
+        textvariable=quant_idxs[r],
+        font=tkFont.Font(family='Space Mono', size=10),
+        state='disabled',
+    ))
+
+    quantities[r].pack()
+    prods[r].configure(command=lambda item=prod_idxs[r]: showIncrementItem(item))
+    prods[r].pack(anchor='w')
     r+=1
     
 
